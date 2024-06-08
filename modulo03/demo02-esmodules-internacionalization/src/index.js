d9585bb87ad7ed5950cdaab0767872788d0b3b69
Marcos Andrade <marcospandrade2@gmail.com>
2024-06-08 11:55:53 -0300

    feat: demo02-esmodules

---
+++ modulo03/demo02-esmodules-internacionalization/src/index.js
@@ -0,0 +1,32 @@
+import DrafLog from 'draftlog';
+import chalk from 'chalk';
+import chalkTable from 'chalk-table';
+import readline from 'readline'
+import database from '../database.json'
+import Person from './person.js';
+
+DrafLog(console).addLineListener(process.stdin);
+
+const options = {
+    leftPad: 2,
+    columns: [
+        { field: 'id', name: chalk.cyan("ID") },
+        { field: 'vehicles', name: chalk.magenta("Vehicles") },
+        { field: 'kmTraveled', name: chalk.cyan("Km Traveled") },
+        { field: 'from', name: chalk.cyan("From") },
+        { field: 'to', name: chalk.cyan("To") }
+    ]
+}
+
+
+const table = chalkTable(options, database.map(item => new Person(item).formatted('pt-BR')));
+const print = console.draft(table)
+
+const terminal = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+terminal.question("TESTE?", msg => {
+    console.log('\nmsg', msg)
+} )
